271f101988c43f862abe088eb1205bfa2be43ade
Dario Pilori <d.pilori@inrim.it>
2021-08-11 16:29:55 +0200

    Add option to avoid generating ISC DHCPd configuration files

--- vlan_config_generator.py
+++ vlan_config_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Script to automatically generate a set of DHCPd configuration files.
+# Script to synchronize a FreeRADIUS database and and ISC DHCPd configuration from
+# Google Sheet files.
 #
 # Copyright (c) 2021 Istituto Nazionale di Ricerca Metrologica <d.pilori@inrim.it>
 #
@@ -35,7 +36,9 @@ def get_vlan_from_json(dct):
     return Vlan(**dct)
    
 # Parse command line arguments
-cli_parser = argparse.ArgumentParser(description="Generate a set of ISC DHCPd configuration files and synchronize FreeRADIUS from Google Sheets files.")
+cli_parser = argparse.ArgumentParser(description="Script to synchronize a FreeRADIUS database and and ISC DHCPd configuration from Google Sheet files.")
+cli_parser.add_argument('--dhcp', help='Generate ISC DHCPd configuration files.', action='store_true')
+cli_parser.add_argument('--no-radius', help='Do not synchronize with FreeRADIUS database.', action='store_true')
 cli_parser.add_argument("-o", "--output-dir",
                        help="Output dir for DHCPd configuration files.", metavar="DIR", default=".")
 cli_parser.add_argument("-c", "--list-vlans",
@@ -49,15 +52,15 @@ cli_parser.add_argument("-v", "--verbose",
 args = cli_parser.parse_args()
 
 # Set up logging
-dhcp_logger = logging.getLogger('dhcp_logger')
-dhcp_logger.setLevel(logging.INFO)
+vlan_logger = logging.getLogger('vlan_logger')
+vlan_logger.setLevel(logging.INFO)
 handler = logging.handlers.TimedRotatingFileHandler(args.log_file, when='W0', backupCount=10)
 handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s:%(message)s'))
-dhcp_logger.addHandler(handler)
+vlan_logger.addHandler(handler)
 if args.verbose:
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s:%(message)s'))
-    dhcp_logger.addHandler(handler)
+    vlan_logger.addHandler(handler)
 
 # Load configuration file
 with open(args.list_vlans, 'r') as f:
@@ -65,25 +68,27 @@ with open(args.list_vlans, 'r') as f:
 
 # For all VLANS
 for v in list_vlan:
-    # Get info from Google Drive
+    # Get data from Google Sheets
     try:
          v.retrieve_data()
     except Exception as exc:
         logging.error('Unable to retrieve data from Google for VLAN {} due to error "{}"'.format(v.vlan_id, exc))
 
-    # Generate and save DHCP configuration
-    try:
-        v.generate_dhcp_config()
-        v.dump_to_dhcpd(out_dir=args.output_dir)
-        dhcp_logger.info('Successfully parsed VLAN {}'.format(v.vlan_id))
-    except Exception as exc:
-        dhcp_logger.error('Skipping DHCP config of vlan {} due to {} error: "{}".'.format(v.vlan_id, type(exc).__name__, exc))
+    # Generate ISC DHCPd configuration files
+    if args.dhcp:
+        try:
+            v.generate_dhcp_config()
+            v.dump_to_dhcpd(out_dir=args.output_dir)
+            vlan_logger.info('Successfully parsed VLAN {}'.format(v.vlan_id))
+        except Exception as exc:
+            vlan_logger.error('Skipping ISC DHCP config of VLAN {} due to {} error: "{}".'.format(v.vlan_id, type(exc).__name__, exc))
 
-    # Generate and save VLAN configuration
-    try:
-        v.generate_radius_config()
-        with open(args.mysql_settings, 'r') as f:
-            mysql_settings = json.load(f)
-        v.dump_to_radius_mysql(**mysql_settings, print_function=dhcp_logger.info)
-    except Exception as exc:
-        dhcp_logger.error('Skipping RADIUS config of vlan {} due to {} error: "{}".'.format(v.vlan_id, type(exc).__name__, exc))
+    # Synchronize with FreeRADIUS MySQL database
+    if not args.no_radius:
+        try:
+            v.generate_radius_config()
+            with open(args.mysql_settings, 'r') as f:
+                mysql_settings = json.load(f)
+            v.dump_to_radius_mysql(**mysql_settings, print_function=vlan_logger.info)
+        except Exception as exc:
+            vlan_logger.error('Skipping RADIUS database sync of VLAN {} due to {} error: "{}".'.format(v.vlan_id, type(exc).__name__, exc))
